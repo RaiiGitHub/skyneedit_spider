97b0244fc342f6f9e2be8d3487573661bd85cdc5
Raii <aiiyizero@163.ocm>
2017-07-25 20:41:06 +0800

    add test.to file.

--- runplan.sh
+++ runplan.sh
@@ -2,4 +2,4 @@
 echo "will run the fetching program(tianyc.node.js.TEST)..."
 echo "May show errors.Just ignore it..."
 cd /data/tianyc
-node test.js
+node main.js 1000-per-time
--- runplantest.sh
+++ runplantest.sh
@@ -2,8 +2,4 @@
 echo "will run the fetching program(tianyc.node.js)..."
 echo "May show errors.Just ignore it..."
 cd /data/tianyc
-node main.js 1000-per-time
-node main.js 1000-per-time
-node main.js 1000-per-time
-node main.js 1000-per-time
-node main.js 1000-per-time
+node test.js
