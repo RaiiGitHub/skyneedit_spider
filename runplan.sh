6d01bce0049824ea922353eded81fb5341eb8143
Raii <aiiyizero@163.ocm>
2017-07-26 20:37:41 +0800

    change to a name

--- runplan.sh
+++ runplan.sh
@@ -2,12 +2,12 @@
 echo "will run the fetching program(tianyc.node.js)..."
 cd /data/tianyc
 #!/bin/sh
-w = `ps -fe|grep tianyc |grep -v grep|wc -l`
-echo $w
-if (($w <= 30))
+TYC=`ps -fe|grep tianyc |grep -v grep|wc -l|awk '{print $1}'`
+echo $TYC
+if (($TYC < 31))
 then
     echo "clean processes....."
-	PROCESS=`ps -ef|grep tianyc|grep -v grep|grep -v PPID|awk '{ print $2}'`
+	PROCESS=`ps -ef|grep main_tianyc|grep -v grep|grep -v PPID|awk '{ print $2}'`
     for i in $PROCESS
     do
         echo "Kill the $1 process [ $i ]"
@@ -16,6 +16,6 @@ then
     echo "clean proxy....."
     node main_releaseproxy.js
 fi
-echo "start main.js in 30 process....."
+echo "start main_tianyc.js in 30 process....."
 #####
 node main.js 30
