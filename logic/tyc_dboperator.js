e082bc4122db8dd657a97295d770b83598d80890
Raii <aiiyizero@163.ocm>
2017-07-26 15:10:37 +0800

    using cluster to support multi-processes,add local file to read.

--- logic/tyc_dboperator.js
+++ logic/tyc_dboperator.js
@@ -61,23 +61,6 @@ class DbOperatorTYC extends dbop {
                         `searchStartTime` timestamp NULL DEFAULT '0000-00-00 00:00:00' COMMENT '搜索开始时间',\
                         `searchEndtTime` timestamp NULL DEFAULT '0000-00-00 00:00:00' COMMENT '搜索结束时间');";
 
-        var search_tasks_define = "CREATE TABLE IF NOT EXISTS `search_tasks_define`(\
-                        `id` int auto_increment primary key COMMENT '自增长的键值',\
-                        `size` int NOT NULL COMMENT '任务堆的大小（单个搜索任务进行的任务量）',\
-                        `code` VARCHAR(45) NULL COMMENT '标识搜索任务的代号',\
-                        `status` VARCHAR(45) NULL COMMENT '运行状态-running,not start',\
-                        `createTime` timestamp NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间');";
-
-        var search_tasks = "CREATE TABLE IF NOT EXISTS `search_tasks`(\
-                        `id` int auto_increment primary key COMMENT '自增长的键值',\
-                        `fid` int NOT NULL COMMENT '外键（表search_tasks_define）',\
-                        `searchFrom` int NOT NULL COMMENT '搜索关键字的起始索引',\
-                        `searchTo` int NOT NULL COMMENT '搜索关键字的结束索引',\
-                        `status` VARCHAR(45) NULL COMMENT '运行状态-running,not start',\
-                        `executeTimes` int NULL COMMENT '任务执行次数',\
-                        `latestTaskExecuteTime` timestamp NULL DEFAULT '0000-00-00 00:00:00' COMMENT '任务最新的执行时间',\
-                         foreign key(fid) references search_tasks_define(id));";
-
         var self = this;
         this.connection_.query(enterprise_base, function (error, results, fields) {
             if (error) {
@@ -97,187 +80,11 @@ class DbOperatorTYC extends dbop {
                                 callback(false);
                             } else {
                                 log._logR('Mysql::ensureTablesExist', 'table search_keys created.');
-                                self.connection_.query(search_tasks_define, function (error, results, fields) {
-                                    if (error) {
-                                        log._logE('Mysql::ensureTablesExist', 'create table failed.', search_tasks_define, error.stack);
-                                        callback(false);
-                                    } else {
-                                        log._logR('Mysql::ensureTablesExist', 'table search_tasks_define created.');
-                                        self.connection_.query(search_tasks, function (error, results, fields) {
-                                            if (error) {
-                                                log._logE('Mysql::ensureTablesExist', 'create table failed.', search_tasks, error.stack);
-                                                callback(false);
-                                            } else {
-                                                log._logR('Mysql::ensureTablesExist', 'table search_tasks created.');
-                                                callback(true);
-                                            }
-                                        });
-                                    }
-                                });
+                                callback(true);
                             }
                         });
                     }
                 });
-            }
-        });
-    }
-
-    verifySearchTasksDefineExist(code, callback) {
-        if (!this.check()) {
-            callback(false);
-            return;
-        }
-        var q = printf('select 1 from search_tasks_define where code = \'%s\' limit 1;', code);
-        this.connection_.query(q, function (error, results, fields) {
-            if (!error) {
-                callback(results.length == 1);
-            } else {
-                log._logE('Mysql::verifySearchTasksDefineExist', q, error.stack);
-                callback(false);
-            }
-        });
-    }
-
-    insertSearchTasksDefine(limit, code, callback) {
-        //this will return the null or the inserted id.
-        if (limit <= 0 || !this.check()) {
-            callback(null);
-            return;
-        }
-        var self = this;
-        self.verifySearchTasksDefineExist(code, function (exist) {
-            if (exist) {
-                log._logE('Mysql::insertSearchTasksDefine', code, 'already exists');
-                callback(null);
-            } else {
-                var q = printf("insert into search_tasks_define(size,code,status,createTime) values(%d,'%s','not start',NOW());", limit, code);
-                self.connection_.query(q, function (error, results, fields) {
-                    if (!error) {
-                        //try to return the neccessarry datas.
-                        self.getSearchCount(function (keysNum) {
-                            if (keysNum) {
-                                callback({ keysNum: keysNum, insertId: results.insertId });//will return the insertid.
-                            } else {
-                                log._logE('Mysql::insertSearchTasksDefine', q, error.stack);
-                                callback(null);
-                            }
-                        });
-                    } else {
-                        log._logE('Mysql::insertSearchTasksDefine', q, error.stack);
-                        callback(null);
-                    }
-                });
-            }
-        });
-    }
-
-    insertSearchTaskUnit(item, callback) {
-        if (!this.check()) {
-            callback(false);
-            return;
-        }
-        var q = printf("insert into search_tasks(fid,searchFrom,searchTo,status,executeTimes) values(%d,%d,%d,'not start',0);", item.fid, item.from, item.to);
-        this.connection_.query(q, function (error, results, fields) {
-            if (!error) {
-                callback(true);
-            } else {
-                log._logE('Mysql::insertSearchTaskUnit', q, error.stack);
-                log._logR('Mysql::insertSearchTaskUnit', q, error.stack);
-                callback(false);
-            }
-        });
-    }
-
-    removeSearchTasks(code, callback) {
-        if (!this.check()) {
-            callback(false);
-            return;
-        }
-        var q = printf("delete from search_tasks_define where code = '%s';", code);//will delete all task-units.
-        this.connection_.query(q, function (error, results, fields) {
-            if (!error) {
-                callback(true);
-            } else {
-                log._logE('Mysql::removeSearchTasks', q, error.stack);
-                callback(false);
-            }
-        });
-    }
-
-    buildSearchTasks(limit, code, callback) {
-        if (limit <= 0 || !this.check()) {
-            callback(false);
-            return;
-        }
-        var self = this;
-        var tasks = [];
-        var insert_datas = [];
-        var final_ok = true;
-        self.insertSearchTasksDefine(limit, code, function (nd) {
-            if (nd) {
-                //insertID is the current count.
-                log._logR('Mysql::buildSearchTasks', nd);
-                var task_assign_func = function (cb) {
-                    if (0 == insert_datas.length) {
-                        callback(final_ok);
-                        cb(null);
-                    } else {
-                        var item = insert_datas.splice(0, 1)[0];
-                        self.insertSearchTaskUnit(item, function (ok) {
-                            if (ok) {
-                                log._logR('Mysql::buildSearchTasks', JSON.stringify(item), 'inserted.');
-                                if (0 == insert_datas.length) {
-                                    callback(final_ok);
-                                }
-                                cb(null);
-                            } else {
-                                //manually rollback.
-                                log._logE('Mysql::buildSearchTasks', JSON.stringify(item), 'insert failed.');
-                                log._logR('Mysql::buildSearchTasks', JSON.stringify(item), 'insert failed.');
-                                final_ok = false;
-                                self.removeSearchTasks(code, function (remove_ok) {
-                                    insert_datas = [];//empty it.
-                                    callback(false);
-                                    cb(null);
-                                });
-                            }
-                        });
-                    }
-                };
-                for (var i = 1; i < nd.keysNum;) {
-                    var item = {
-                        fid: nd.insertId,
-                        from: i,
-                        to: Math.min((i + limit - 1), nd.keysNum)
-                    };
-                    insert_datas.push(item);
-                    tasks.push(function (callback) {
-                        task_assign_func(callback);
-                    });
-                    i = item.to + 1;
-                }
-                async.waterfall(tasks, function (error, result) {
-                    //run the waterfall right now!
-                });
-            } else {
-                log._logE('Mysql::buildSearchTasks', 'No insertID, build tasks failed.');
-                callback(false);
-            }
-        });
-    }
-
-    verifySearchKeyExists(key, callback) {
-        if (!this.check()) {
-            callback(false);
-            return;
-        }
-        var q = printf('select 1 from search_keys where searchKey = \'%s\' limit 1;', key);
-        this.connection_.query(q, function (error, results, fields) {
-            if (!error) {
-                callback(results.length == 1);
-            } else {
-                log._logE('Mysql::verifySearchKeyExists', q, error.stack);
-                callback(false);
             }
         });
     }
@@ -307,7 +114,7 @@ class DbOperatorTYC extends dbop {
                             if (error) {
                                 log._logE('Mysql::insertSeachKeys', 'exist,or unknown...', q);
                                 callback(false);
-                            }else{
+                            } else {
                                 console.log(q);
                             }
                         });
@@ -338,6 +145,21 @@ class DbOperatorTYC extends dbop {
         });
     }
 
+    verifySearchKeyExists(key, callback) {
+        if (!this.check()) {
+            callback(false);
+            return;
+        }
+        var q = printf('select 1 from search_keys where searchKey = \'%s\' limit 1;', key);
+        this.connection_.query(q, function (error, results, fields) {
+            if (!error) {
+                callback(results.length == 1);
+            } else {
+                log._logE('Mysql::verifySearchKeyExists', q, error.stack);
+                callback(false);
+            }
+        });
+    }
 
     updateSearchKeyStatus(key, status, callback) {
         var self = this;
@@ -358,25 +180,6 @@ class DbOperatorTYC extends dbop {
         });
     }
 
-    updateSearchTaskStatus(id, status, callback) {
-        var self = this;
-        if (!self.check()) {
-            callback(false);
-            return;
-        }
-        var etc = status == 'running' ? ',latestTaskExecuteTime=NOW(),executeTimes=executeTimes+1' : '';
-        var q = printf("update search_tasks a set status = '%s'%s \
-                        where a.id = %d", status, etc, id);
-        self.connection_.query(q, function (error, results, fields) {
-            if (error) {
-                log._logE('Mysql::updateSearchTaskStatus', q, error.stack);
-                callback(false);
-            } else {
-                callback(true);
-            }
-        });
-    }
-
     getSearchKeys(from, count, callback) {
         if (!this.check()) {
             callback(null);
@@ -385,7 +188,7 @@ class DbOperatorTYC extends dbop {
         var limit = count ? ('limit ' + count) : '';
         var q = printf("SELECT searchKey FROM search_keys where id >= %d %s;", from, limit);
         this.connection_.query(q, function (error, results, fields) {
-            if (!error) {
+            if (!error && results.length > 0) {
                 callback(results);
             } else {
                 log._logE('Mysql::getSearchKeys', q, error.stack);
@@ -410,29 +213,6 @@ class DbOperatorTYC extends dbop {
         });
     }
 
-    getSearchTaskUnitToRun(code, callback) {
-        if (!this.check()) {
-            callback(null);
-            return;
-        }
-        //will return the minimal searchFrom item.
-        var q = printf("SELECT id,searchFrom, searchTo FROM search_tasks WHERE searchFrom = \
-                       (SELECT MIN(a.searchFrom) FROM search_tasks a,search_tasks_define b \
-                       WHERE a.status = 'not start' AND a.fid = b.id AND b.code = '%s');", code);
-        this.connection_.query(q, function (error, results, fields) {
-            if (!error) {
-                if (results.length > 0) {
-                    callback({ id: results[0].id, from: results[0].searchFrom, to: results[0].searchTo });
-                } else {
-                    callback(null);//no datas.
-                }
-            } else {
-                log._logE('Mysql::getSearchTaskUnitToRun', q, error.stack);
-                callback(null);
-            }
-        });
-    }
-
     verifyCompanyExists(code, callback) {
         if (!this.check()) {
             callback(false);
@@ -448,6 +228,7 @@ class DbOperatorTYC extends dbop {
             }
         });
     }
+
     verifyCompanyPageExists(code, callback) {
         if (!this.check()) {
             callback(false);
@@ -463,6 +244,7 @@ class DbOperatorTYC extends dbop {
             }
         });
     }
+
     insertCompany(desc, callback) {
         if (!this.check()) {
             callback(false);
@@ -488,6 +270,7 @@ class DbOperatorTYC extends dbop {
             }
         })
     }
+
     insertCompanyPage(desc, html, callback) {
         if (!this.check()) {
             callback(false);
--- main.js
+++ main.js
@@ -6,13 +6,12 @@ const pv = require('./libs/proxyvisitor');
 const dbop = require('./logic/tyc_dboperator');
 const emitter = require('./libs/searchemitter');
 const log = require('./libs/log');
-const lr = require('line-reader');
 const cluster = require('cluster');
-const async = require('async');
 
 log.init('./datas/logs', 'NORMAL', 'Windows', 'TYCFetcher');
 
 if (cluster.isMaster) {
+  var search_key_index = 1;
   process.on('SIGINT', function () {
     exit = true;
     log._logR('Main::Exit', 'Got SIGINT.  Release all proxies before exiting...');
@@ -22,93 +21,61 @@ if (cluster.isMaster) {
     });
   });
   var process_args = process.argv.splice(2);
-  if (2 > process_args.length) {
-    log._logR('Main::EntryPoint', 'argument code and process-num should be here.');
-    log._logE('Main::EntryPoint', 'argument code and process-num should be here.');
+  if (1 > process_args.length) {
+    log._logR('Main::EntryPoint', 'argument code should be here.');
+    log._logE('Main::EntryPoint', 'argument code should be here.');
     return;
   }
-  var search_code = process_args[0];
-  var process_num = process_args[1];
-  //var numCPUs = require('os').cpus().length;
-  var worker_tasks = process_num;
-  for (var i = 0; i < process_num; i++) {
-    var wp = cluster.fork();
-    wp.on('message', function (msg) {
-      if (msg.onefinished) {
-        worker_tasks--;
-        if (0 >= worker_tasks) {
-          log._logR('Main::Finished', 'all.');
-          process.exit(0);
-        }
-      }
-    });
-    wp.send({ search_code: search_code });
-  }
-} else {
-  process.on('message', function (msg) {
-    var main = null;
-    log._logR('Main::SearchKey', msg.search_code);
-    var db = new dbop();
-    db.config();
-    var tasks = [];
-    var skeys = [];
-    var next_func = function (cb) {
-      if (skeys.length == 0) {
-        cb(null);
-        main();
-      } else {
-        var k = skeys.splice(0, 1)[0];
-        console.log(k);
-        var e = new emitter(
-          new explainer,
-          new dbop,
-          new urlentity(printf('http://www.tianyancha.com/search?key=%s', urlentity.encodeUrl(k)), 1, k)
-        );
-        e.emit(true, function () {
-          log._logR('Main::finished', 'one.');
-          if (skeys.length == 0) {
-            //redo again.
-            log._logR('Main::Continue', 'one more time...');
-            main();
-            cb(null);
-          } else {
-            cb(null);
+
+  var search_key_index_offset = 1;
+  fs.readFile('./sko.txt', 'utf8', function (err, buf) {
+    if (!err) {
+      var process_num = process_args[0];
+      search_key_index_offset = parseInt(buf, 10);
+      //var numCPUs = require('os').cpus().length;
+      var worker_tasks = process_num;
+      for (var i = 0; i < process_num; i++) {
+        var wp = cluster.fork();
+        wp.on('message', function (msg) {
+          if (msg.nomoredata) {
+            worker_tasks--;
+            if (0 >= worker_tasks) {
+              log._logR('Main::Finished', 'all.');
+              process.exit(0);
+            }
+          } else if (msg.next) {
+            search_key_index_offset++;
+            fs.writeFile('./sko.txt', search_key_index_offset, function (err) {
+              console.log('Main::Next', 'Ready go oto the next...');
+              wp.send({ offset: search_key_index_offset });
+            });
           }
         });
+        wp.send({ offset: search_key_index_offset });
       }
-    };
-    var main = function (cb_main) {
-      db.getSearchTaskUnitToRun(msg.search_code, function (runitem) {
-        if (runitem) {
-          db.updateSearchTaskStatus(runitem.id, 'running', function (ok) {
-            if (ok) {
-              log._logR('Main::Run', msg.search_code, 'Ready to run,from', runitem.from, 'to', runitem.to);
-              task_size = runitem.to - runitem.from + 1;
-              db.getSearchKeys(runitem.from, runitem.to, function (keys) {
-                if (keys) {
-                  for (var k in keys) {
-                    skeys.push(keys[k].searchKey);
-                    tasks.push(function (callback) {
-                      next_func(callback);
-                    });
-                  }
-                  async.waterfall(tasks, function (error, result) {
-                  });
-                }
-              });
-            } else {
-              log._logR('Main::Run', 'task not found.', msg.search_code);
-              log._logE('Main::Run', 'task not found.', msg.search_code);
-              process.send({ onefinished: true });
-            }
+    }
+  })
+} else {
+  process.on('message', function (msg) {
+    if (msg.offset) {
+      var db = new dbop();
+      db.config();
+      db.getSearchKeys(msg.offset, 1, function (results) {
+        if (results) {
+          var k = results[0].searchKey;
+          var e = new emitter(
+            new explainer,
+            new dbop,
+            new urlentity(printf('http://www.tianyancha.com/search?key=%s', urlentity.encodeUrl(k)), 1, k)
+          );
+          e.emit(true, function () {
+            log._logR('Main::finished', 'one.');
+            process.send({next:true});
           });
-        } else {
-          log._logR('Main::Run', 'No data to run', msg.search_code);
-          log._logE('Main::Run', 'No data to run', msg.search_code);
-          process.send({ onefinished: true });
+        }else{
+          process.send({nomoredata:true});//no more datas.
         }
       });
     }
-    main();
   });
 } 
